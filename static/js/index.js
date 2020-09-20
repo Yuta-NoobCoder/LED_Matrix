9f5480f240055dd3fe51ff9744bbd268da3bee20
Yuta-NoobCoder <ukiyutaM4A1@gmail.com>
2020-09-20 16:17:18 +0100

    メニュー画面の一部ボタン処理を実装

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,10 @@
-var btn_next = document.getElementById("next");
-var btn_prev = document.getElementById("prev");
-var current_image = document.getElementById("led");
+var btn_rollsign = document.getElementById("btn_rollsign");
+var btn_signboard = document.getElementById("btn_signboard");
 
-btn_next.addEventListener('click', function(){
-    xhr("/rollsign/action?type=next");
-});
+btn_rollsign.addEventListener("click", function(){
+    location.href = "/rollsign";
+})
 
-btn_prev.addEventListener('click', function(){
-    xhr("/rollsign/action?type=prev");
-});
-
-window.onload = function () {
-    //開始時の画像を取得
-    xhr("/rollsign/action?type=image");
-}
-
-function xhr(path){
-    var request = new XMLHttpRequest();
-    request.open('GET', path, true);
-    request.onload = function(){
-        console.log(request.responseText);
-        current_image.src = request.responseText;
-    }
-    request.send();
-}
+btn_signboard.addEventListener("click", function(){
+    location.href = "/signboard";
+})
