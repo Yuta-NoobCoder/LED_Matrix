1d111bac34a977a48145dca8b690ddb21b468438
Yuta-NoobCoder <ukiyutaM4A1@gmail.com>
2020-09-21 05:01:23 +0100

    自動切り替え機能の実装

--- static/js/rollsign.js
+++ static/js/rollsign.js
@@ -1,5 +1,9 @@
-var btn_next = document.getElementById("next");
-var btn_prev = document.getElementById("prev");
+var btn_next = document.getElementById("btn_next");
+var btn_prev = document.getElementById("btn_prev");
+var chk_auto_switch = document.getElementById("chk_auto_switch"); //自動切り替え
+var txt_interval = document.getElementById("txt_interval"); //間隔
+var select_interval = document.getElementById("select_interval"); //単位
+var lbl_interval = document.getElementById("lbl_interval");
 var current_image = document.getElementById("led");
 
 btn_next.addEventListener('click', function(){
@@ -8,6 +12,29 @@ btn_next.addEventListener('click', function(){
 
 btn_prev.addEventListener('click', function(){
     xhr("/rollsign/action?type=prev");
+});
+
+var timer;
+chk_auto_switch.addEventListener('change', function(){
+    if(chk_auto_switch.checked == true)
+    {
+        txt_interval.disabled = false;
+        select_interval.disabled = false;
+        lbl_interval.style.opacity = 1.0;
+        //タイマーをスタート
+        timer = setInterval(function(){
+            xhr("/rollsign/action?type=next");
+        }, txt_interval.value * 1000);
+        console.log(txt_interval.value * 1000);
+    }
+    else
+    {
+        txt_interval.disabled = true;
+        select_interval.disabled = true;
+        lbl_interval.style.opacity = 0.5;
+        //タイマーを停止
+        clearInterval(timer);
+    }
 });
 
 function xhr(path){
